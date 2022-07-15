f03bc72593f246f3175e966611121684c372fe9a
JenMcD-star <jenniferannvancura@gmail.com>
2022-07-15 12:39:18 -0500

    Made an array for the display value to fix only one number showing on the display at a time (no double digits).

--- calculator.js
+++ calculator.js
@@ -1,23 +1,33 @@
-
-let displayValue = '0';
-let firstOperand = null;
-let secondOperand = null;
-let firstOperator = null;
-let secondOperator = null;
-let result = null;
 const buttons = document.querySelectorAll('#input');
+let displayValue = [];
+let num1 = null;
+let num2 = null;
+let operator1 = null;
+let operator2 = null;
+let result = null;
 //get value from button click
 //save values
+
 //display clicked buttons
-
    buttons.forEach((input) => {
-    input.addEventListener('click', function(event) {   
-    let displayValue = event.target.value
-      document.getElementById("display").innerHTML = displayValue
+    input.addEventListener('click', function(event) { 
+     displayValue.push(event.target.value)
+     console.log(displayValue)
+     newValue = +displayValue.join("")
+     document.getElementById("display").innerHTML = newValue;
     });
-   })   
-   
+  })   
 
+
+   
+   function clearDisplay() {
+    displayValue = '0';
+    firstOperand = null;
+    secondOperand = null;
+    firstOperator = null;
+    secondOperator = null;
+    result = null;
+   }
 const operate = function(operator, x, y) {
     switch(operator) {
      case "+":
