9c6d0b206e53b771ed55910aec4540d200b5285d
JenMcD-star <jenniferannvancura@gmail.com>
2022-07-15 14:58:04 -0500

    Added variables in the number and operator buttons to save the x and y variables and save the operator value

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
-const buttons = document.querySelectorAll('button.number');
-const operatorBtns = document.querySelectorAll('button.operator');
+const buttons = document.querySelectorAll("button.number");
+const operatorBtns = document.querySelectorAll("button.operator");
 let displayValue = [];
+let newValue = null;
 let x = null;
 let y = null;
 let operator1 = null;
@@ -10,45 +11,60 @@ let result = null;
 //save values
 
 //display clicked buttons
-   buttons.forEach((input) => {
-    input.addEventListener('click', function(event) { 
-     displayValue.push(event.target.value)
-     console.log(displayValue)
-     newValue = +displayValue.join("")
-     document.getElementById("display").innerHTML = newValue;
-    
-    });
-  })   
-
-  operatorBtns.forEach((operator) => {
-    operator.addEventListener('click', function(event) { 
-     document.getElementById("display").innerHTML = '0';
-      displayValue.push(event.target.value)
-     document.getElementById("display").innerHTML = displayValue;
-    });
-  })
-  
-
-   
-   function clearDisplay() {
-    displayValue =[];
-    clearValue = '0';
-    document.getElementById("display").innerHTML = clearValue;
-    firstOperand = null;
-    secondOperand = null;
-    firstOperator = null;
-    secondOperator = null;
-    result = null;
-   }
-const operate = function(operator, x, y) {
-    switch(operator) {
-     case "+":
-       return x + y;
-     case "-":
-       return x-y;
-     case "*":
-       return x*y;
-    case "/":
-       return x / y;
+buttons.forEach((input) => {
+  input.addEventListener("click", function (event) {
+    displayValue.push(event.target.value);
+    console.log(displayValue);
+    newValue = +displayValue.join("");
+    document.getElementById("display").innerHTML = newValue;
+    if (x != null && y === null) {
+      y = newValue;
+      console.log(y);
     }
+  });
+});
+
+operatorBtns.forEach((operator) => {
+  operator.addEventListener("click", function (event) {
+    if (x === null) {
+      x = newValue;
+      console.log(newValue);
+    }
+    if (operator1 === null) {
+      operator1 = this.value;
+      console.log(operator1);
+    }
+    document.getElementById("display").innerHTML = event.target.value;
+    displayValue = [];
+  });
+});
+//if the first operator is not null then do the operation
+function clearDisplay() {
+  displayValue = [];
+  clearValue = "0";
+  document.getElementById("display").innerHTML = clearValue;
+  firstOperand = null;
+  secondOperand = null;
+  firstOperator = null;
+  secondOperator = null;
+  result = null;
 }
+const operate = function (operator, x, y) {
+  switch (operator) {
+    case "+":
+      result = x + y;
+      return result;
+    case "-":
+      result = x - y;
+      return result;
+    case "*":
+      result = x * y;
+      return result;
+    case "/":
+      result = x / y;
+      return result;
+  }
+  displayValue = [];
+  totalValue = result;
+  document.getElementById("display").innerHTML = result;
+};
