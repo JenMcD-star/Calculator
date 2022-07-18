f692f02289b5be54d7227be61f7ba4f934268dc0
JenMcD-star <jenniferannvancura@gmail.com>
2022-07-18 17:28:00 -0500

    Addede sescondOperator variable

--- calculator.js
+++ calculator.js
@@ -5,6 +5,7 @@ let newValue = null;
 let x = null;
 let y = null;
 let operator = null;
+let secondOperator = null;
 let result = null;
 
 buttons.forEach((input) => {
@@ -17,29 +18,43 @@ buttons.forEach((input) => {
       y = secondNum;
       console.log(y);
     }
+    if (x != null && y != null && operator != null) {
+      operate(operator, x, y);
+    }
   });
 });
 
 operatorBtns.forEach((operator1) => {
   operator1.addEventListener("click", function (event) {
+    // assigns first input to a variable
     if (x === null) {
       firstNum = newValue;
       x = firstNum;
       console.log(x);
     }
-     if (x != null && y != null && operator != null) {
+    // if there is already a value for both numbers and the operator then operate
+    if (x != null && y != null && operator != null) {
       operate(operator, x, y);
-    } else if (operator === null) {
+    }
+    // if the operator doesn't have a value then take the value from the button
+    if (operator === null) {
       tempOperator = this.value;
       operator = tempOperator;
-      console.log(operator);;
-    }
+      console.log(operator);
+    // if there is already an operator replace x with result of previous operation and save new operator
+    } else if (operator != null){
+      tempOperator = this.value;
+      secondOperator = tempOperator;
+      console.log(secondOperator);
+      x = result;
+      y = null;
 
+    }
     document.getElementById("display").innerHTML = event.target.value;
     displayValue = [];
   });
 });
-
+//resets back to initial calculator
 function clearDisplay() {
   displayValue = [];
   clearValue = "0";
@@ -49,9 +64,6 @@ function clearDisplay() {
   operator = null;
   result = null;
 }
-
-
-
 
 const operate = function (op, num1, num2) {
   if (op === "+") {
@@ -69,14 +81,13 @@ const operate = function (op, num1, num2) {
   }
   document.getElementById("display").innerHTML = result;
   x = result;
-  console.log(x)
+  console.log(x);
   y = null;
-  console.log(y)
 };
 
 function backspace() {
   let backValue = displayValue.pop();
   backValue = +displayValue.join("");
   newValue = backValue;
-  console.log(newValue);
+  document.getElementById("display").innerHTML = newValue;
 }
