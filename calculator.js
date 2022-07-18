f6b78d6caf3ee4f03fd144793da18489c37c3e8d
JenMcD-star <jenniferannvancura@gmail.com>
2022-07-18 13:38:14 -0500

    Added a reset to the y variable at the end of the operate function and made x = result in the operate function.

--- calculator.js
+++ calculator.js
@@ -2,8 +2,6 @@ const buttons = document.querySelectorAll("button.number");
 const operatorBtns = document.querySelectorAll("button.operator");
 let displayValue = [];
 let newValue = null;
-let firstOperator = null;
-let secondOperator = null;
 let x = null;
 let y = null;
 let operator = null;
@@ -14,30 +12,29 @@ buttons.forEach((input) => {
     displayValue.push(event.target.value);
     newValue = +displayValue.join("");
     document.getElementById("display").innerHTML = newValue;
-    })
+    if (x != null) {
+      secondNum = newValue;
+      y = secondNum;
+      console.log(y);
+    }
   });
+});
 
 operatorBtns.forEach((operator1) => {
   operator1.addEventListener("click", function (event) {
     if (x === null) {
       firstNum = newValue;
-      x = firstNum
+      x = firstNum;
       console.log(x);
-        }    
-    else if (x != null) {
-      secondNum = newValue ;
-      y = secondNum;
-      console.log(y)
     }
-      
-    if (operator === null) {
-      operator = this.value;
-      console.log(operator);
+     if (x != null && y != null && operator != null) {
+      operate(operator, x, y);
+    } else if (operator === null) {
+      tempOperator = this.value;
+      operator = tempOperator;
+      console.log(operator);;
     }
-    
-  if (x && y) {
-   operate(operator, x, y);
-}
+
     document.getElementById("display").innerHTML = event.target.value;
     displayValue = [];
   });
@@ -53,23 +50,28 @@ function clearDisplay() {
   result = null;
 }
 
-const equals = function(){}
-const operate = function (operator, num1, num2) {
-  if (operator === "+") {
+
+
+
+const operate = function (op, num1, num2) {
+  if (op === "+") {
     result = num1 + num2;
     return result;
-  } else if (operator === "-") {
+  } else if (op === "-") {
     result = num1 - num2;
     return result;
-  } else if (operator === "*") {
+  } else if (op === "*") {
     result = num1 * num2;
     return result;
-  } else if (operator === "/") {
+  } else if (op === "/") {
     result = num1 / num2;
     return result;
   }
-  console.log(result);
   document.getElementById("display").innerHTML = result;
+  x = result;
+  console.log(x)
+  y = null;
+  console.log(y)
 };
 
 function backspace() {
